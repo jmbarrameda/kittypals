54c338fd060564992c3ac428b4749c3149e6e3fa
Jaycee Barrameda <jmbarrameda@up.edu.ph>
2020-09-24 23:21:19 +0800

    Updates

---
+++ precache-manifest.bac9dc1ef551c75f0833781f23002099.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1688639e03f62d5d8ba8cb012a3894fc",
+    "url": "/kittypals/index.html"
+  },
+  {
+    "revision": "e2bcca10b3d5603b660a",
+    "url": "/kittypals/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0dc38007fbe28b378196",
+    "url": "/kittypals/static/css/main.2fe4fa3b.chunk.css"
+  },
+  {
+    "revision": "e2bcca10b3d5603b660a",
+    "url": "/kittypals/static/js/2.1e120568.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittypals/static/js/2.1e120568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc38007fbe28b378196",
+    "url": "/kittypals/static/js/main.fea8f125.chunk.js"
+  },
+  {
+    "revision": "470c84ad1db2e9944c82",
+    "url": "/kittypals/static/js/runtime-main.a659ab72.js"
+  }
+]);
